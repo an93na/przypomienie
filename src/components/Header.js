f2e95cb03a4dc2316debd2448cde74dcaa9007e4
Anna Giermakowska <an.giermakowska@gmail.com>
2026-03-17 20:21:15 +0100

    stylowanie paska nawigacji

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import classes from '../style/link.module.css'
 export const Header = () => {
   return (
-    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
+    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignContent: 'center', alignItems: 'center', backgroundColor: 'blueviolet' }}>
         <Link to="/" className={classes.link}>STRONA GŁÓWNA</Link>
         <Link to="strona1" className={classes.link}>STRONA1</Link>
         <Link to="strona2" className={classes.link}>STRONA2</Link>
