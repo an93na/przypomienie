6bcf8ad6dfee666a268d89ef1e4a6c1aa0d53023
Anna Giermakowska <an.giermakowska@gmail.com>
2026-03-18 19:39:40 +0100

    testy liczydła

--- src/components/Strona3.js
+++ src/components/Strona3.js
@@ -9,9 +9,11 @@ export const Strona3 = () => {
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia esse repellat vel culpa ducimus cumque eligendi magnam non, hic eius!
       </p>
       <div>
-        <button onClick={() => setValue(value + 1)}>+</button>
-        <input type="text" value={value} onChange={(e) => setValue(Number(e.target.value))}/>
-        <button onClick={() => setValue(value - 1)}>-</button>
+        <button style={{marginRight:'2vw', marginBottom: '2vw'}} onClick={() => setValue(value + 1)}>+1</button>
+        <button style={{marginRight:'2vw', marginBottom: '2vw'}} onClick={() => setValue(value + 5)}>+5</button>
+        <input type="text" id='1' value={value} onChange={(e) => setValue(Number(e.target.value))}/>
+        <button style={{marginRight:'2vw', marginBottom: '2vw'}} onClick={() => setValue(value - 1)}>-1</button>
+        <button style={{marginRight:'2vw', marginBottom: '2vw'}} onClick={() => setValue(value - 5)}>-5</button>
       </div>
     </article>
   )
