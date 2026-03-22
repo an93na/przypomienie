9d8f926e182a75e8e03e9a1af8fd5fef17e4b0e7
Anna Giermakowska <an.giermakowska@gmail.com>
2026-03-22 20:30:00 +0100

    zmiany w pliku strona4.js

--- src/components/Strona4.js
+++ src/components/Strona4.js
@@ -19,6 +19,18 @@ export const Strona4 = () => {
                 <div style={{ width: '40px', height: '40px', backgroundColor: 'blue', borderRadius: '50%', display: 'inline-flex', justifyContent: "center", alignItems: "center", }}>2</div>
                 <div style={{ width: '60px', height: '60px', backgroundColor: 'green', borderRadius: '50%', display: 'inline-flex', textAlign: 'center', alignContent: 'center', justifyContent: 'center', alignItems: 'center' }}>3</div>
             </div>
+            <div>
+                <div style={{ backgroundColor: 'bisque', width: '120px', height: '130px', borderRadius: '10px', paddingTop: '15px', marginTop: 10 }}>
+                    <div style={{ backgroundColor: 'darkblue', borderRadius: '10px', padding: '10px' }}>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', alignContent: 'center', alignItems: 'center'}}>
+                            <p>Work</p>
+                            <p>...</p>
+                        </div>
+                        <h4>32hrs</h4>
+                        <p style={{ fontSize: '8px', textDecoration: '' }}>Last Week - 36hrs</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
